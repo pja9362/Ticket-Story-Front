ec83b0f163e7d6ee11f3266c468d955750a22516
jina <pja9362@g.skku.edu>
2023-11-23 11:23:51 +0900

    feat(setting): ios 상태바 고려 SafeArea 적용

--- frontend/App.js
+++ frontend/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 import InitScreen from './src/pages/Auth/Init';
 import LoginScreen from './src/pages/Auth/Login';
 import SignUpScreen from './src/pages/Auth/SignUp';
@@ -16,65 +17,44 @@ import MainStack from './src/navigation/MainStack';
 const App = () => {
   const Stack = createNativeStackNavigator();
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Init"
-          options={{headerShown: false}}
-          component={InitScreen}
-        />
-        <Stack.Screen
-          name="Login"
-          options={{headerShown: false}}
-          component={LoginScreen}
-        />
-        <Stack.Screen
-          name="SignUp"
-          options={{headerShown: false}}
-          component={SignUpScreen}
-        />
-        <Stack.Screen name="MainStack" options={{headerShown: false}}>
-          {({navigation}) => <MainStack navigation={navigation}/>}
-        </Stack.Screen>
+  const screenOptions = {
+    headerShown: false,
+  };
 
-        <Stack.Screen
-          name="EnrollByOCRAgreement"
-          options={{headerShown: false}}
-          component={EnrollByOCRAgreement}
-        />
-        <Stack.Screen
-          name="EnrollByHandAgreement"
-          options={{headerShown: false}}
-          component={EnrollInfoByHandAgreement}
-        />
-        <Stack.Screen
-          name="EnrollInfoByHand"
-          options={{headerShown: false}}
-          component={EnrollInfoByHand}
-        />
-        <Stack.Screen
-          name="EnrollReviewByHand"
-          options={{headerShown: false}}
-          component={EnrollReviewByHand}
-        />
-        <Stack.Screen
-          name="EnrollFinish"
-          options={{headerShown: false}}
-          component={EnrollFinish}
-        />
-        <Stack.Screen
-          name="EnrollInfoByOCR"
-          options={{headerShown: false}}
-          component={EnrollInfoByOCR}
-        />
-        <Stack.Screen
-          name="OCRFail"
-          options={{headerShown: false}}
-          component={OCRFail}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView edges={['top']} style={{flex: 1}}>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen name="Init" component={InitScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="SignUp" component={SignUpScreen} />
+            <Stack.Screen name="MainStack">
+              {({navigation}) => <MainStack navigation={navigation} />}
+            </Stack.Screen>
+            <Stack.Screen
+              name="EnrollByOCRAgreement"
+              component={EnrollByOCRAgreement}
+            />
+            <Stack.Screen
+              name="EnrollByHandAgreement"
+              component={EnrollInfoByHandAgreement}
+            />
+            <Stack.Screen
+              name="EnrollInfoByHand"
+              component={EnrollInfoByHand}
+            />
+            <Stack.Screen
+              name="EnrollReviewByHand"
+              component={EnrollReviewByHand}
+            />
+            <Stack.Screen name="EnrollFinish" component={EnrollFinish} />
+            <Stack.Screen name="EnrollInfoByOCR" component={EnrollInfoByOCR} />
+            <Stack.Screen name="OCRFail" component={OCRFail} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
 

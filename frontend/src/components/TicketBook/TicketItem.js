4215950c0318ab31610c2ca46372630f85735cdc
jina <pja9362@g.skku.edu>
2024-03-29 01:04:14 +0900

    feat(design): 티켓북 티켓 디자인

--- frontend/src/components/TicketBook/TicketItem.js
+++ frontend/src/components/TicketBook/TicketItem.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { View, Text, ImageBackground, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, ImageBackground, StyleSheet, Dimensions, Image } from 'react-native';
 
 import goldenTicket from '../../images/ticketbook_gold.png';
 import silverTicket from '../../images/ticketbook_silver.png';
 import bronzeTicket from '../../images/ticketbook_bronze.png';
 import basicTicket from '../../images/ticketbook_basic.png';
 
-const TicketItem = ({ type, review, photo, contentRating, seatRating, category, date, location, seat }) => {
+import iconReviewOn from '../../images/icon_ReviewOn.png';
+import iconReviewOff from '../../images/icon_ReviewOff.png';
+import iconPhotoOn from '../../images/icon_PhotoOn.png';
+import iconPhotoOff from '../../images/icon_PhotoOff.png';
+
+const TicketItem = ({ type, title, review, photo, contentRating, seatRating, date, category, location, seat }) => {
   let ticketImageSource;
   switch (type) {
     case 'gold':
@@ -26,16 +31,36 @@ const TicketItem = ({ type, review, photo, contentRating, seatRating, category, 
     <ImageBackground source={ticketImageSource} style={styles.imageBackground} resizeMode="contain">
       <View style={styles.textContentContainer}>
         <View style={styles.leftContent}>
-          <Text style={styles.review}>{review}</Text>
-          <Text style={styles.photo}>{photo}</Text>
-          <Text style={styles.contentRating}>콘텐츠 평점: {contentRating}</Text>
-          <Text style={styles.seatRating}>좌석 평점: {seatRating}</Text>
+          <View style={{flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 10}}>
+            <View style={{alignItems: 'center'}}>
+              <Text style={styles.guideText}>리뷰</Text>
+              <Image source={review !== '' ? iconReviewOn : iconReviewOff} style={styles.icon}/>
+            </View>
+            <View style={{alignItems: 'center'}}>
+              <Text style={styles.guideText}>사진</Text>
+              <Image source={photo !== '' ? iconPhotoOn : iconPhotoOff} style={styles.icon}/>
+            </View>
+          </View>
+          <View style={{flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 10}}>
+            <View style={{alignItems: 'center'}}>
+              <Text style={styles.guideText}>콘텐츠</Text>
+              <Text style={styles.rate}>{contentRating}</Text>
+            </View>
+            <View style={{alignItems: 'center'}}>
+              <Text style={styles.guideText}>좌석</Text>
+              <Text style={styles.rate}>{seatRating}</Text>
+            </View>
+          </View>
         </View>
         <View style={styles.rightContent}>
-          <Text style={styles.category}>카테고리: {category}</Text>
-          <Text style={styles.date}>일시: {date}</Text>
-          <Text style={styles.location}>장소: {location}</Text>
-          <Text style={styles.seat}>좌석: {seat}</Text>
+          <View style={{flexDirection: 'row', alignItems: 'center', gap: 5, marginBottom: 8}}>
+            <View style={{height: 17, width: 5, backgroundColor: category === 'MUSICAL'?  '#DC0000' :  category === 'MOVIE' ? '#6B00F6' : category === 'SPORTS' ? '#03C75A' : '#FF5C00' }} />
+            <Text style={styles.category}>{title}</Text>
+          </View>
+
+          <Text style={styles.infoText}>일시: {date}</Text>
+          <Text style={styles.infoText}>장소: {location}</Text>
+          <Text style={styles.infoText}>좌석: {seat}</Text>
         </View>
       </View>
     </ImageBackground>
@@ -54,113 +79,48 @@ const styles = StyleSheet.create({
   },
   textContentContainer: {
     flexDirection: 'row',
-    paddingHorizontal: 20,
+    paddingHorizontal: 15,
   },
   leftContent: {
     flex: 1,
-    paddingRight: 10,
+    marginRight: 12,
+    gap: 3,
   },
   rightContent: {
     flex: 2,
-    paddingLeft: 10,
+    paddingLeft: 12,
+    justifyContent: 'center',
+  },
+  guideText: {
+    fontSize: 12,
+    color: '#9A9A9A',
+    fontWeight: 'bold',
+    marginBottom: 5,
+    textAlign: 'center',
+  },
+  rate: {
+    fontSize: 19,
+    color: '#525252',
+    fontWeight: 'bold',
+    marginBottom: 5,
+    textAlign: 'center',
+  },
+  icon: {
+    width: 28,
+    height: 28,
+    marginBottom: 5,
+  },
+  infoText: {
+    fontSize: 14,
+    color: '#525252',
+    marginBottom: 5,
+    fontWeight: 'bold',
+  },
+  category: {
+    fontSize: 16,
+    color: '#525252',
+    fontWeight: 'bold',
   },
 });
 
 export default TicketItem;
-
-// import React from 'react';
-// import { View, Text, Image, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
-
-// import goldenTicket from '../../images/ticketbook_gold.png';
-// import silverTicket from '../../images/ticketbook_silver.png';
-// import bronzeTicket from '../../images/ticketbook_bronze.png';
-// import basicTicket from '../../images/ticketbook_basic.png';
-
-// const TicketItem = ({ type, review, photo, contentRating, seatRating, category, date, location, seat }) => {
-//   let ticketImageSource;
-//   switch (type) {
-//     case 'gold':
-//       ticketImageSource = goldenTicket;
-//       break;
-//     case 'silver':
-//       ticketImageSource = silverTicket;
-//       break;
-//     case 'bronze':
-//       ticketImageSource = bronzeTicket;
-//       break;
-//     default:
-//       ticketImageSource = basicTicket;
-//   }
-
-//   return (
-//     <View style={styles.container}>
-//       <Image source={ticketImageSource} style={styles.ticketImage} resizeMode="contain" />
-//       {/* Text Content */}
-//       <View style={styles.textContent}>
-//         <View style={styles.leftContent}>
-//             <Text style={styles.review}>{review}</Text>
-//             <Text style={styles.photo}>{photo}</Text>
-//             <Text style={styles.contentRating}>콘텐츠 평점: {contentRating}</Text>
-//             <Text style={styles.seatRating}>좌석 평점: {seatRating}</Text>
-//         </View>
-//         <View style={styles.rightContent}>
-//             <Text style={styles.category}>카테고리: {category}</Text>
-//             <Text style={styles.date}>일시: {date}</Text>
-//             <Text style={styles.location}>장소: {location}</Text>
-//             <Text style={styles.seat}>좌석: {seat}</Text>
-//         </View>
-//       </View>
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     marginBottom: 20,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     backgroundColor: 'red'
-//   },
-//   ticketImage: {
-//     width: Dimensions.get('window').width -26,
-//   },
-//   textContent: {
-//     position: 'absolute',
-//     justifyContent: 'space-between',
-//     flexDirection: 'row',
-//     left: 20,
-//     right: 20,
-//   },
-//   leftContent: {
-//     flex: 1,
-//   },
-//   rightContent: {
-//     flex: 1.5,
-//   },
-//   review: {
-//     marginBottom: 5,
-//   },
-//   photo: {
-//     marginBottom: 5,
-//   },
-//   contentRating: {
-//     marginBottom: 5,
-//   },
-//   seatRating: {
-//     marginBottom: 5,
-//   },
-//   category: {
-//     marginBottom: 5,
-//   },
-//   date: {
-//     marginBottom: 5,
-//   },
-//   location: {
-//     marginBottom: 5,
-//   },
-//   seat: {
-//     marginBottom: 5,
-//   },
-// });
-
-// export default TicketItem;

075e33b677d7f4f4d019ce4536fb67733e3326f0
jina <pja9362@g.skku.edu>
2024-05-10 04:58:52 +0900

    feat(design): 짜잘한 디자인 수정

--- frontend/src/pages/EnrollTicket/EnrollFinish.js
+++ frontend/src/pages/EnrollTicket/EnrollFinish.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import {View, Image, StyleSheet, Text, Button} from 'react-native';
+import {View, Image, StyleSheet, Text, Button, TouchableOpacity} from 'react-native';
 import ticket from '../../images/ticket.png';
-
-
 const EnrollFinish = ({navigation}) => {
     return (
         <View style={styles.container}>
@@ -10,20 +8,16 @@ const EnrollFinish = ({navigation}) => {
             <Text style={styles.mainText}>나의 티켓스토리가 등록되었어요.</Text>
             <Text style={styles.subText}>등록한 티켓은 티켓북에서 확인할 수 있어요.</Text>
 
-
             <View style={styles.buttonContainer}>
-                <Button
-                    style={styles.button}
-                    title="홈으로 가기"
-                    onPress={() => navigation.navigate('MainStack')} />
-                <Button 
-                    style={styles.button}
-                    title="티켓북 보기"
-                    onPress={() => navigation.navigate('TicketBook')} />
-                <Button
-                    style={styles.button}
-                    title="티켓 추가 등록하기"
-                    onPress={() => {}} />
+                <TouchableOpacity style={{...styles.navButton, backgroundColor: '#5D70F9' }} onPress={() => navigation.navigate('MainStack')}>
+                    <Text style={{...styles.btnText, color: '#fff'}}>홈으로 가기</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.navButton} onPress={() => navigation.navigate('TicketBook')}>
+                    <Text style={styles.btnText}>티켓북으로 가기</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.navButton} onPress={() => navigation.navigate('EnrollTicket')}>
+                    <Text style={styles.btnText}>티켓 등록하기</Text>
+                </TouchableOpacity>
             </View>
         </View>
     )
@@ -52,8 +46,21 @@ const styles = StyleSheet.create({
         color: '#000',
     },
     buttonContainer: {
-        marginTop: 40,
+        marginTop: '20%',
         paddingHorizontal: 40,
+        width: '100%',
+        gap: 16
+    },
+    navButton: {
+        backgroundColor: '#EEEEEE',
+        paddingHorizontal: 18,
+        paddingVertical: 15,
+        borderRadius: 10,
+    },
+    btnText: {
+        color: '#000',
+        fontSize: 18,
+        textAlign: 'center',
     },
 });
 

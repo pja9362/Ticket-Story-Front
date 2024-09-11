83110b2f3834b88acaa7c9b0121e401e0dc2fcd6
jina <pja9362@g.skku.edu>
2024-09-11 23:08:55 +0900

    feat(drawer): 사이드바 메뉴 약관 외부링크 연결

--- frontend/src/pages/CustomDrawerContent.js
+++ frontend/src/pages/CustomDrawerContent.js
@@ -10,12 +10,17 @@ import { logoutRequest } from '../actions/auth/auth';
 import { useDispatch } from 'react-redux';
 import { jwtDecode } from "jwt-decode";
 import "core-js/stable/atob";
+import {WebView} from 'react-native-webview';
+import backButton from '../images/back_button.png';
 
 const CustomDrawerContent = ( props ) => {
 
   const navigation = useNavigation();
 
   const [modalVisible, setModalVisible] = useState(false); 
+
+  const [webViewVisible, setWebViewVisible] = useState(false);
+  const [webViewUrl, setWebViewUrl] = useState('');
 
   const handleLogout = async () => {
     try {
@@ -110,70 +115,96 @@ const CustomDrawerContent = ( props ) => {
     }
   }
 
+
+  const handleLabelPress = (idx) => {
+    let url = '';
+    switch (idx) {
+      case 1: // 서비스 이용약관
+        url = 'https://sugar-dresser-cb0.notion.site/16ea3dbb19534ea3a14a567254f63168?pvs=4';
+        break;
+      case 2: // 개인정보 처리방침
+        url = 'https://app.catchsecu.com/document/C/9a77778561d8ddc';
+        break;
+      default:
+        break;
+    }
+    if (url) {
+      setWebViewUrl(url);
+      setWebViewVisible(true);
+    }
+  }
   
   return (
     <>
-    <TouchableOpacity onPress={() => props.navigation.closeDrawer()} style={styles.closeButton}>
-      <Image source={iconx} style={{...styles.closeIcon, width: 25, height: 25,}}/>
-    </TouchableOpacity>
-
-    <DrawerContentScrollView {...props}>
-    <View style={{ flex: 1 }}>
-      {/* Navigation Items */}
-      <CustomText style={{marginLeft:20, fontSize : 18, color:"#525252"}} fontWeight="bold">이용 안내</CustomText>
-      {/* <TouchableOpacity onPress={() => console.log('공지사항 클릭')} style={styles.menuItem}> */}
-      <TouchableOpacity onPress={() => navigation.navigate('NoticeList')} style={styles.menuItem}>
-        <CustomText style={styles.menuText} fontWeight="medium">공지사항</CustomText>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('AskScreen')} style={styles.menuItem}>
-        <CustomText style={styles.menuText} fontWeight="medium">문의하기</CustomText>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('ServiceScreen')} style={styles.menuItem}>
-        <CustomText style={styles.menuText} fontWeight="medium">서비스 이용약관</CustomText>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('PrivacyScreen')} style={styles.menuItem}>
-        <CustomText style={styles.menuText} fontWeight="medium">개인정보 처리방침</CustomText>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('LicenseList')} style={styles.menuEndItem}>
-        <CustomText style={styles.menuText} fontWeight="medium">오픈소스 라이선스</CustomText>
+      <TouchableOpacity onPress={() => props.navigation.closeDrawer()} style={styles.closeButton}>
+        <Image source={iconx} style={{...styles.closeIcon, width: 25, height: 25,}}/>
       </TouchableOpacity>
 
-      <CustomText style={{marginLeft:20, fontSize : 18, marginTop: 25, color:"#525252"}} fontWeight="bold">계정 관리</CustomText>
-      <TouchableOpacity onPress={handleChangePW} style={styles.menuItem}>
-        <CustomText style={styles.menuText} fontWeight="medium">비밀번호 변경</CustomText>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => setModalVisible(true)} style={styles.menuItem}>
-      {/* <TouchableOpacity onPress={() => navigation.navigate('Init')} style={styles.menuItem}> */}
-        <CustomText style={styles.menuText} fontWeight="medium">로그아웃</CustomText>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={handleQuit} style={styles.menuItem}>
-        <CustomText style={styles.menuText} fontWeight="medium">회원 탈퇴</CustomText>
-      </TouchableOpacity>
+      <DrawerContentScrollView {...props}>
+      <View style={{ flex: 1 }}>
+        {/* Navigation Items */}
+        <CustomText style={{marginLeft:20, fontSize : 18, color:"#525252"}} fontWeight="bold">이용 안내</CustomText>
+        {/* <TouchableOpacity onPress={() => console.log('공지사항 클릭')} style={styles.menuItem}> */}
+        <TouchableOpacity onPress={() => navigation.navigate('NoticeList')} style={styles.menuItem}>
+          <CustomText style={styles.menuText} fontWeight="medium">공지사항</CustomText>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('AskScreen')} style={styles.menuItem}>
+          <CustomText style={styles.menuText} fontWeight="medium">문의하기</CustomText>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleLabelPress(1)} style={styles.menuItem}>
+          <CustomText style={styles.menuText} fontWeight="medium">서비스 이용약관</CustomText>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleLabelPress(2)} style={styles.menuItem}>
+          <CustomText style={styles.menuText} fontWeight="medium">개인정보 처리방침</CustomText>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('LicenseList')} style={styles.menuEndItem}>
+          <CustomText style={styles.menuText} fontWeight="medium">오픈소스 라이선스</CustomText>
+        </TouchableOpacity>
 
-    </View>
-    </DrawerContentScrollView>
+        <CustomText style={{marginLeft:20, fontSize : 18, marginTop: 25, color:"#525252"}} fontWeight="bold">계정 관리</CustomText>
+        <TouchableOpacity onPress={handleChangePW} style={styles.menuItem}>
+          <CustomText style={styles.menuText} fontWeight="medium">비밀번호 변경</CustomText>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setModalVisible(true)} style={styles.menuItem}>
+        {/* <TouchableOpacity onPress={() => navigation.navigate('Init')} style={styles.menuItem}> */}
+          <CustomText style={styles.menuText} fontWeight="medium">로그아웃</CustomText>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleQuit} style={styles.menuItem}>
+          <CustomText style={styles.menuText} fontWeight="medium">회원 탈퇴</CustomText>
+        </TouchableOpacity>
 
-    <Modal  
-      // animationType="slide"
-      transparent={true}
-      visible={modalVisible}
-      onRequestClose={() => setModalVisible(false)}
-    >
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
-        <View style={{ backgroundColor: 'white', width: 280, padding: 15, borderRadius: 10 }}>
-          <CustomText style={{color: '#525252', fontSize: 16, textAlign: 'center', marginTop: 5}} fontWeight="bold">로그아웃 하시겠어요?</CustomText>
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 20 }}>
-            <TouchableOpacity onPress={() => setModalVisible(false)} style={{ backgroundColor: '#E8ECEF', width: 120, padding: 10, borderRadius: 12 }}>
-              <CustomText style={{ color: '#000', textAlign : 'center', fontSize: 16}} fontWeight="medium">취소</CustomText>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={handleLogout} style={{ backgroundColor: '#5D70f9', width: 120, padding: 10, borderRadius: 12 }}>
-            {/* <TouchableOpacity onPress={() => navigation.replace('Init')} style={{ backgroundColor: '#5D70f9', width: 100, padding: 10, borderRadius: 5 }}> */}
-              <CustomText style={{ color: 'white', textAlign : 'center', fontSize: 16}} fontWeight="medium">확인</CustomText>
-            </TouchableOpacity>
+      </View>
+      </DrawerContentScrollView>
+
+      <Modal  
+        // animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+          <View style={{ backgroundColor: 'white', width: 280, padding: 15, borderRadius: 10 }}>
+            <CustomText style={{color: '#525252', fontSize: 16, textAlign: 'center', marginTop: 5}} fontWeight="bold">로그아웃 하시겠어요?</CustomText>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 20 }}>
+              <TouchableOpacity onPress={() => setModalVisible(false)} style={{ backgroundColor: '#E8ECEF', width: 120, padding: 10, borderRadius: 12 }}>
+                <CustomText style={{ color: '#000', textAlign : 'center', fontSize: 16}} fontWeight="medium">취소</CustomText>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={handleLogout} style={{ backgroundColor: '#5D70f9', width: 120, padding: 10, borderRadius: 12 }}>
+              {/* <TouchableOpacity onPress={() => navigation.replace('Init')} style={{ backgroundColor: '#5D70f9', width: 100, padding: 10, borderRadius: 5 }}> */}
+                <CustomText style={{ color: 'white', textAlign : 'center', fontSize: 16}} fontWeight="medium">확인</CustomText>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
-      </View>
-    </Modal>
+      </Modal>
+
+      {/* WebView Modal */}
+      <Modal visible={webViewVisible} onRequestClose={() => setWebViewVisible(false)}>
+          <TouchableOpacity style={{paddingTop: 16, paddingBottom: 10}} onPress={() => setWebViewVisible(false)}>
+            <Image source={backButton} style={{width: 28, height: 28, marginHorizontal: 20}}/>
+          </TouchableOpacity>
+          <WebView source={{ uri: webViewUrl }} />
+      </Modal>
     </>
   );
 };
